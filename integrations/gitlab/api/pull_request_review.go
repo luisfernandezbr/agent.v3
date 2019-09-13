abfcdb6e431ed193988030e7b8754efcd1b7f4ad
Jose C Ordaz <cordaz@pinpoint.com>
2019-09-13 11:38:45 -0500

    getting the right date when a reviewer approves a pr

--- integrations/gitlab/api/pull_request_review.go
+++ integrations/gitlab/api/pull_request_review.go
@@ -10,6 +10,42 @@ import (
 	"github.com/pinpt/integration-sdk/sourcecode"
 )
 
+func ApprovedDate(qc QueryContext, repoID string, prIID string, username string) (unixDate int64, err error) {
+
+	qc.Logger.Debug("approval date", "repo", repoID, "prIID", prIID)
+
+	objectPath := pstrings.JoinURL("projects", repoID, "merge_requests", prIID, "discussions")
+
+	var discussions []struct {
+		Notes []struct {
+			Body      string    `json:"body"`
+			CreatedAt time.Time `json:"created_at"`
+			Author    struct {
+				Username string `json:"username"`
+			} `json:"author"`
+		} `json:"notes"`
+	}
+
+	_, err = qc.Request(objectPath, nil, &discussions)
+	if err != nil {
+		return
+	}
+
+	var date time.Time
+
+	for _, discussion := range discussions {
+		for _, note := range discussion.Notes {
+			if note.Body == "approved this merge request" && note.Author.Username == username && note.CreatedAt.After(date) {
+				date = note.CreatedAt
+			}
+		}
+	}
+
+	unixDate = date.Unix()
+
+	return
+}
+
 func PullRequestReviewsPage(
 	qc QueryContext,
 	repo Repo,
@@ -45,7 +81,10 @@ func PullRequestReviewsPage(
 		item.CustomerID = qc.CustomerID
 		item.RefType = qc.RefType
 		item.RefID = fmt.Sprint(rreview.ID)
-		item.UpdatedAt = rreview.UpdatedAt.Unix()
+		item.UpdatedAt, err = ApprovedDate(qc, repo.ID, pr.IID, a.Username)
+		if err != nil {
+			return
+		}
 		item.RepoID = qc.RepoID(repo.ID)
 		item.PullRequestID = qc.PullRequestID(item.RepoID, pr.ID)
 		item.State = sourcecode.PullRequestReviewStateApproved
