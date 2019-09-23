035f74dbdfab30c2c7a0fe833a6cdce84f425445
Andris Valums <andris.valums@pinpt.com>
2019-09-23 23:06:49 +0200

    service: allow extra integration to run in service-run when receiving export trigger

--- pkg/agentconf/conf.go
+++ pkg/agentconf/conf.go
@@ -18,6 +18,12 @@ type Config struct {
 	// SystemID normally does not change across installs on the same machine. But to be safe we keep it in config as well.
 	SystemID        string `json:"system_id"`
 	PPEncryptionKey string `json:"pp_encryption_key"`
+
+	// ExtraIntegrations defines additional integrations that will run on every export trigger in service-run. This is needed to run a custom integration for one of our customers. You need to add these custom integrations to config manually after enroll.
+	ExtraIntegrations []struct {
+		Name   string                 `json:"name"`
+		Config map[string]interface{} `json:"config"`
+	} `json:"extra_integrations"`
 }
 
 func Save(c Config, loc string) error {
