be54f96c25b43a4ea52e1ffec8825f3bee2dda7a
Jose C Ordaz <cordaz@pinpoint.com>
2019-10-09 16:31:32 -0500

    Revert "Revert "APP-2109 Added WorkStatusRequest for jira""
    This reverts commit 281d9d2a7d78db6e12a82050b1c16aca36930faf.

--- cmd/commands.go
+++ cmd/commands.go
@@ -125,7 +125,7 @@ var cmdExportOboardData = &cobra.Command{
 			if v == "" {
 				exitWithErr(logger, errors.New("provide object-type arg"))
 			}
-			if v == "users" || v == "repos" || v == "projects" {
+			if v == "users" || v == "repos" || v == "projects" || v == "workconfig" {
 				opts.ExportType = rpcdef.OnboardExportType(v)
 			} else {
 				exitWithErr(logger, fmt.Errorf("object-type must be one of: users, repos, projects, got %v", v))
