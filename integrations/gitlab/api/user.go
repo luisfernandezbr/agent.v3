2a2e14ad4999e61e5aff450235c130e0616762a1
Jose C Ordaz <cordaz@pinpoint.com>
2019-10-15 15:43:57 -0500

    deleted unused code

--- integrations/gitlab/api/user.go
+++ integrations/gitlab/api/user.go
@@ -10,7 +10,6 @@ import (
 	"github.com/pinpt/integration-sdk/sourcecode"
 
 	pstrings "github.com/pinpt/go-common/strings"
-	"github.com/pinpt/integration-sdk/agent"
 )
 
 type User struct {
@@ -19,51 +18,6 @@ type User struct {
 	Username  string
 	Name      string
 	AvatarURL string
-}
-
-func GroupUsers(qc QueryContext, group string) (users []*agent.UserResponseUsers, err error) {
-	qc.Logger.Debug("fetching users", "group", group)
-
-	objectPath := pstrings.JoinURL("/groups/", url.PathEscape(group), "/members/all")
-
-	var rawUsers []struct {
-		ID        int    `json:"id"`
-		Name      string `json:"name"`
-		Username  string `json:"username"`
-		AvatarURL string `json:"avatar_url"`
-	}
-
-	_, err = qc.Request(objectPath, nil, &rawUsers)
-	if err != nil {
-		return users, err
-	}
-
-	for _, user := range rawUsers {
-		nUser := &agent.UserResponseUsers{
-			RefID:      fmt.Sprint(user.ID),
-			RefType:    qc.RefType,
-			CustomerID: qc.CustomerID,
-			Username:   user.Username,
-			AvatarURL:  pstrings.Pointer(user.AvatarURL),
-			Active:     true,
-			Name:       user.Name,
-			Groups: []agent.UserResponseUsersGroups{
-				agent.UserResponseUsersGroups{
-					Name: group,
-				},
-			},
-		}
-
-		nUser.Emails, err = emailsUser(qc, nUser.Username)
-		if err != nil {
-			return
-		}
-
-		users = append(users, nUser)
-
-	}
-
-	return users, nil
 }
 
 func emailsUser(qc QueryContext, username string) (emails []string, err error) {
