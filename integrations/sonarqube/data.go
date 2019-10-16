4897d734b2b4f0f97bf8a6bb33a07a4a1dbae61f
Pedro Enrique <penrique@pinpoint.com>
2019-10-16 18:37:55 +0200

    return on errors

--- integrations/sonarqube/data.go
+++ integrations/sonarqube/data.go
@@ -1,70 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/pinpt/agent.next/integrations/pkg/objsender2"
 	"github.com/pinpt/integration-sdk/codequality"
 )
 
 func (s *Integration) exportAll() error {
-	session, err := objsender2.Root(s.agent, "sonarqube")
-	if err != nil {
-		s.logger.Error("error creating root tracking for sonarqube", "err", err)
-		return err
-	}
 	projects, err := s.api.FetchProjects()
 	if err != nil {
 		s.logger.Error("error fetching projects", "err", err)
 		return err
 	}
-	projsession, err := session.Session(codequality.ProjectModelName.String(), "root", "root")
+	session, err := objsender2.Root(s.agent, codequality.ProjectModelName.String())
 	if err != nil {
 		s.logger.Error("error creating project session", "err", err)
 		return err
 	}
-	if err := projsession.SetTotal(len(projects)); err != nil {
+	if err := session.SetTotal(len(projects)); err != nil {
 		s.logger.Error("error setting total projects on exportAll", "err", err)
+		return err
 	}
-	var errors []string
 	for _, project := range projects {
 		project.CustomerID = s.customerID
-		if err := projsession.Send(project); err != nil {
+		if err := session.Send(project); err != nil {
 			s.logger.Error("error sending project to agent", "err", err, "id", project.RefID)
-			continue
+			return err
 		}
 		metricsession, err := session.Session(codequality.MetricModelName.String(), project.RefID, project.Name)
 		if err != nil {
 			s.logger.Error("error creating metric session", "err", err)
-			continue
+			return err
 		}
 		metrics, err := s.api.FetchMetrics(project, session.LastProcessedTime())
 		if err != nil {
-			if err := metricsession.Done(); err != nil {
-				errors = append(errors, err.Error())
-			}
 			s.logger.Error("error fetching metrics", "err", err)
-			continue
+			return err
 		}
 		for _, metric := range metrics {
 			metric.CustomerID = s.customerID
 			if err := metricsession.Send(metric); err != nil {
 				s.logger.Error("error sending metric to agent", "err", err, "id", metric.RefID)
+				return err
 			}
 		}
 		if err := metricsession.Done(); err != nil {
-			errors = append(errors, err.Error())
+			return err
 		}
-	}
-	if err := projsession.Done(); err != nil {
-		errors = append(errors, err.Error())
-	}
-	if err := session.Done(); err != nil {
-		errors = append(errors, err.Error())
-	}
-	if errors != nil {
-		return fmt.Errorf("error with session done. %v", strings.Join(errors, ", "))
 	}
 	return nil
 }
