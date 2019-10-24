4ebfdcd256490a1beb0cf3fb84dd8e67cdb52d96
Pedro Enrique <penrique@pinpoint.com>
2019-10-24 21:23:45 +0200

    test empty string as well

--- integrations/azuretfs/api/work_user_test.go
+++ integrations/azuretfs/api/work_user_test.go
@@ -16,4 +16,11 @@ func TestDoubleSlashRegex(t *testing.T) {
 	test2 := doubleSlashRegex.ReplaceAllString(str2, "")
 	assert.Equal(t, test2, "Pedro Enrique")
 
+	str3 := `Pedro Enrique`
+	test3 := doubleSlashRegex.ReplaceAllString(str3, "")
+	assert.Equal(t, test3, "Pedro Enrique")
+
+	str4 := ``
+	test4 := doubleSlashRegex.ReplaceAllString(str4, "")
+	assert.Equal(t, test4, "")
 }
