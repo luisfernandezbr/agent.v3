3a6f83b8d160a19abf5c4d0a42a470af2796256e
Jeff Haynie <jhaynie@pinpoint.com>
2019-11-14 22:13:29 -0600

    fixed issue with gzip files in zip

--- cmd/cmdexport/export.go
+++ cmd/cmdexport/export.go
@@ -19,7 +19,7 @@ import (
 	"github.com/pinpt/agent.next/pkg/integrationid"
 	"github.com/pinpt/agent.next/pkg/memorylogs"
 
-	"github.com/hashicorp/go-plugin"
+	plugin "github.com/hashicorp/go-plugin"
 	"github.com/pinpt/agent.next/cmd/cmdintegration"
 	"github.com/pinpt/agent.next/pkg/jsonstore"
 	"github.com/pinpt/agent.next/rpcdef"
@@ -214,7 +214,7 @@ func (s *export) tempFilesInUploads() ([]string, error) {
 				res = append(res, sr...)
 				continue
 			}
-			if !strings.HasSuffix(n, ".temp") {
+			if !strings.HasSuffix(n, ".temp.gz") {
 				continue
 			}
 			res = append(res, n)
