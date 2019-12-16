65576c6ec3da3ddd8a9a87cc83dba835d3571457
Pedro Enrique <penrique@pinpoint.com>
2019-12-16 23:24:07 +0100

    don’t exit when agent is cancelled

--- pkg/interrupt/interrupt.go
+++ pkg/interrupt/interrupt.go
@@ -3,11 +3,30 @@
 package interrupt
 
 import (
+	"fmt"
 	"os"
+	"os/signal"
 	"syscall"
 )
 
+var listening = false
+
 func Kill(process *os.Process) error {
+
+	if !listening {
+		listening = true
+		ctrlc := make(chan os.Signal, 1)
+		signal.Notify(ctrlc, os.Interrupt)
+
+		go func() {
+			select {
+			case s := <-ctrlc:
+				fmt.Println("control c catched", s)
+				signal.Reset(os.Interrupt)
+			}
+		}()
+	}
+
 	pgid, err := syscall.Getpgid(process.Pid)
 	if err != nil {
 		return err
--- pkg/interrupt/interrupt_windows.go
+++ pkg/interrupt/interrupt_windows.go
@@ -3,12 +3,31 @@
 package interrupt
 
 import (
+	"fmt"
 	"os"
+	"os/signal"
 
 	"golang.org/x/sys/windows"
 )
 
+var listening = false
+
 func Kill(process *os.Process) error {
+
+	if !listening {
+		listening = true
+		ctrlc := make(chan os.Signal, 1)
+		signal.Notify(ctrlc, os.Interrupt)
+
+		go func() {
+			select {
+			case s := <-ctrlc:
+				fmt.Println("control c catched", s)
+				signal.Reset(os.Interrupt)
+			}
+		}()
+	}
+
 	dll, err := windows.LoadDLL("kernel32.dll")
 	if err != nil {
 		return err
