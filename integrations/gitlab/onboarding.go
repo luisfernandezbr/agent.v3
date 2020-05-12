1ee23005549bc932e55ca93e02de205cbd5c9df4
Pedro Enrique <pedro.tma@gmail.com>
2020-05-12 17:50:31 +0200

    Missed onboard (#329)

--- integrations/gitlab/onboarding.go
+++ integrations/gitlab/onboarding.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/go-hclog"
 	"github.com/pinpt/agent/integrations/gitlab/api"
 	"github.com/pinpt/agent/rpcdef"
+	"github.com/pinpt/integration-sdk/agent"
 )
 
 func (s *Integration) OnboardExport(ctx context.Context, objectType rpcdef.OnboardExportType, config rpcdef.ExportConfig) (res rpcdef.OnboardExportResult, _ error) {
@@ -15,15 +16,37 @@ func (s *Integration) OnboardExport(ctx context.Context, objectType rpcdef.Onboa
 		return res, err
 	}
 	switch objectType {
-	case rpcdef.OnboardExportTypeRepos:
-		return s.onboardExportRepos(ctx)
+	case rpcdef.OnboardExportTypeRepos, rpcdef.OnboardExportTypeProjects:
+		return s.onboardExportRepos(ctx, objectType)
+	case rpcdef.OnboardExportTypeWorkConfig:
+		return s.onboardWorkConfig(ctx, config.Integration.ID)
 	default:
 		res.Error = rpcdef.ErrOnboardExportNotSupported
 		return
 	}
 }
 
-func (s *Integration) onboardExportRepos(ctx context.Context) (res rpcdef.OnboardExportResult, _ error) {
+func (s *Integration) onboardWorkConfig(ctx context.Context, intid string) (res rpcdef.OnboardExportResult, _ error) {
+
+	ws := &agent.WorkStatusResponseWorkConfig{}
+	ws.CustomerID = s.customerID
+	ws.IntegrationID = intid
+	ws.RefType = "gitlab"
+	ws.Statuses = agent.WorkStatusResponseWorkConfigStatuses{
+		OpenStatus:       []string{"open", "Open"},
+		InProgressStatus: []string{"in progress", "In progress", "In Progress"},
+		ClosedStatus:     []string{"closed", "Closed"},
+	}
+	ws.TopLevelIssue = agent.WorkStatusResponseWorkConfigTopLevelIssue{
+		Name: "Issue",
+		Type: "Issue",
+	}
+
+	res.Data = ws.ToMap()
+	return
+}
+
+func (s *Integration) onboardExportRepos(ctx context.Context, objectType rpcdef.OnboardExportType) (res rpcdef.OnboardExportResult, _ error) {
 	groupNames, err := api.GroupsAll(s.qc)
 	if err != nil {
 		return res, err
@@ -40,7 +63,20 @@ func (s *Integration) onboardExportRepos(ctx context.Context) (res rpcdef.Onboar
 				return pi, err
 			}
 			for _, repo := range repos {
-				records = append(records, repo.ToMap())
+
+				if objectType == rpcdef.OnboardExportTypeRepos {
+					records = append(records, repo.ToMap())
+				} else {
+					records = append(records, (&agent.ProjectResponseProjects{
+						Active:            repo.Active,
+						Description:       &repo.Description,
+						Error:             agent.ProjectResponseProjectsError(repo.Error),
+						Name:              repo.Name,
+						RefID:             repo.RefID,
+						RefType:           repo.RefType,
+						WebhookPermission: repo.WebhookPermission,
+					}).ToMap())
+				}
 			}
 			return pi, nil
 		})
