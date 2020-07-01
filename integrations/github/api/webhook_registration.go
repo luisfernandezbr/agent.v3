cf6d1c0b679c5694ffb5bed65f933aaf453e9121
Robindiddams <rdiddams@pinpoint.com>
2020-07-01 12:12:24 -0500

    dont remove webhook if it has v4 integration_id param

--- integrations/github/api/webhook_registration.go
+++ integrations/github/api/webhook_registration.go
@@ -10,6 +10,7 @@ import (
 	"reflect"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/pinpt/agent/pkg/requests"
@@ -70,10 +71,12 @@ func WebhookCreateIfNotExists(qc QueryContext, repo Repo, webhookURL string, eve
 		})
 
 		for _, wh := range pinptWebHooks[1:] {
-			err := webhookRemove(qc, repo, wh.ID)
-			if err != nil {
-				rerr = err
-				return
+			if !strings.Contains(wh.Config.URL, "?integration_instance_id") {
+				err := webhookRemove(qc, repo, wh.ID)
+				if err != nil {
+					rerr = err
+					return
+				}
 			}
 		}
 	}
