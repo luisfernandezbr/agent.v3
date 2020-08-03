bf7eccf2c90e6bee3e0e518f586accc9f0198a34
Pedro Enrique <penrique@pinpoint.com>
2020-08-03 18:11:06 +0200

    GOLD-206 use refresh tokens to get calendar

--- integrations/gcal/integration.go
+++ integrations/gcal/integration.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"errors"
-	"net/url"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/pinpt/agent/cmd/cmdrunnorestarts/inconfig"
@@ -39,7 +38,6 @@ type Integration struct {
 	agent   rpcdef.Agent
 	refType string
 	config  IntegrationConfig
-	api     api.API
 }
 
 // NewIntegration _
@@ -60,6 +58,7 @@ func (s *Integration) Init(agent rpcdef.Agent) error {
 type Calendar struct {
 	RefID string
 	Name  string
+	API   api.API
 }
 
 // GetID gets the ref id
@@ -86,9 +85,6 @@ func containsRefID(list []string, one string) bool {
 
 // Export exports all the calendars in the Inclusions list and its events
 func (s *Integration) Export(ctx context.Context, conf rpcdef.ExportConfig) (res rpcdef.ExportResult, _ error) {
-	if err := s.initAPI(conf); err != nil {
-		return res, err
-	}
 	s.logger.Info("starting gcal export")
 	session, err := objsender.Root(s.agent, calendar.CalendarModelName.String())
 	if err != nil {
@@ -99,23 +95,35 @@ func (s *Integration) Export(ctx context.Context, conf rpcdef.ExportConfig) (res
 	session.SetNoAutoProgress(true)
 	var projectsIface []repoprojects.RepoProject
 	if len(s.config.Inclusions) > 0 {
-		for _, each := range s.config.Inclusions {
-			cal, err := s.api.GetCalendar(url.QueryEscape(each))
+		for _, refreshToken := range s.config.Inclusions {
+
+			api, err := api.New(s.logger, conf.Pinpoint.CustomerID, s.refType, func() (string, error) {
+				return s.agent.OAuthNewAccessTokenFromRefreshToken(s.refType, refreshToken)
+			})
+
+			cals, err := api.GetCalendars()
 			if err != nil {
-				s.logger.Warn("error fetching calendar, skipping", "err", err, "id", each)
+				s.logger.Warn("error fetching calendars for user, skipping", "err", err, "refreshToken", refreshToken)
 				continue
 			}
-			if err := session.Send(cal); err != nil {
-				s.logger.Error("error sending event to agent", "err", err, "id", cal.RefID)
-				return res, err
+			for _, cal := range cals {
+				if err := session.Send(cal); err != nil {
+					s.logger.Error("error sending event to agent", "err", err, "id", cal.RefID)
+					return res, err
+				}
+				projectsIface = append(projectsIface, Calendar{
+					RefID: cal.Description, // this is the email, use this to get events
+					Name:  cal.Name,
+					API:   api,
+				})
 			}
-			projectsIface = append(projectsIface, Calendar{
-				RefID: cal.Description, // this is the email, use this to get events
-				Name:  cal.Name,
-			})
 		}
 	} else {
-		cals, err := s.api.GetCalendars()
+		api, err := s.initAPI(conf)
+		if err != nil {
+			return res, err
+		}
+		cals, err := api.GetCalendars()
 		if err != nil {
 			s.logger.Error("error fetching calendars", "err", err)
 			return res, err
@@ -146,7 +154,7 @@ func (s *Integration) Export(ctx context.Context, conf rpcdef.ExportConfig) (res
 			return "", err
 		}
 		s.logger.Info("trying to get events for user_id", "user_id", proj.RefID, "name", proj.Name)
-		events, users, nextToken, err := s.api.GetEventsAndUsers(proj.RefID, eventSender.LastProcessed())
+		events, users, nextToken, err := proj.API.GetEventsAndUsers(proj.RefID, eventSender.LastProcessed())
 		if err != nil {
 			s.logger.Error("error fetching events for user_id, skipping", "err", err, "user_id", proj.RefID, "name", proj.Name)
 			return "", err
@@ -202,11 +210,12 @@ func (s *Integration) Export(ctx context.Context, conf rpcdef.ExportConfig) (res
 // ValidateConfig calls a simple api to make sure we have the correct credentials
 func (s *Integration) ValidateConfig(ctx context.Context, conf rpcdef.ExportConfig) (res rpcdef.ValidationResult, _ error) {
 
-	if err := s.initAPI(conf); err != nil {
+	api, err := s.initAPI(conf)
+	if err != nil {
 		res.Errors = append(res.Errors, err.Error())
 		return res, err
 	}
-	if err := s.api.Validate(); err != nil {
+	if err := api.Validate(); err != nil {
 		res.Errors = append(res.Errors, err.Error())
 		s.logger.Info("error with validate", "err", err)
 		return res, err
@@ -217,11 +226,12 @@ func (s *Integration) ValidateConfig(ctx context.Context, conf rpcdef.ExportConf
 // OnboardExport returns the data used in onboard
 func (s *Integration) OnboardExport(ctx context.Context, objectType rpcdef.OnboardExportType, conf rpcdef.ExportConfig) (res rpcdef.OnboardExportResult, _ error) {
 
-	if err := s.initAPI(conf); err != nil {
+	api, err := s.initAPI(conf)
+	if err != nil {
 		res.Error = err
 		return res, err
 	}
-	cals, err := s.api.GetCalendars()
+	cals, err := api.GetCalendars()
 	if err != nil {
 		res.Error = err
 		return res, err
@@ -247,24 +257,23 @@ func (s *Integration) Mutate(ctx context.Context, fn string, data string, conf r
 	return res, errors.New("mutate not supported")
 }
 
-func (s *Integration) initAPI(conf rpcdef.ExportConfig) error {
+func (s *Integration) initAPI(conf rpcdef.ExportConfig) (api.API, error) {
 	if err := structmarshal.MapToStruct(conf.Integration.Config, &s.config); err != nil {
 		s.logger.Error("error creating the config object", "err", err)
-		return err
+		return nil, err
 	}
-	var oauth *oauthtoken.Manager
-	accessToken := s.config.AccessToken
-	if s.config.Local {
-		if accessToken == "" {
-			return errors.New("access token required")
+
+	return api.New(s.logger, conf.Pinpoint.CustomerID, s.refType, func() (string, error) {
+		if s.config.Local {
+			if s.config.AccessToken == "" {
+				return "", errors.New("access token required")
+			}
+			return s.config.AccessToken, nil
 		}
-	} else {
-		var err error
-		oauth, err = oauthtoken.New(s.logger, s.agent)
+		oauth, err := oauthtoken.New(s.logger, s.agent)
 		if err != nil {
-			return err
+			return "", err
 		}
-	}
-	s.api = api.New(s.logger, conf.Pinpoint.CustomerID, s.refType, oauth, accessToken)
-	return nil
+		return oauth.Get(), nil
+	})
 }
